23361233a753b03b55bca57669d612470011e2ed
John Nana <bnicemagnolia@gmail.com>
2022-06-19 22:39:49 +0100

    completes NFT minting function

--- test/NFTBadge.test.js
+++ test/NFTBadge.test.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+const fs = require("fs");
+const path = require("path");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+
 
 describe("NFTBadge",  function () {
   
@@ -11,6 +15,20 @@ describe("NFTBadge",  function () {
 
     
     expect(await badge.owner()).to.equal(owner.address)
+    const svg1 =  fs.readFileSync(`${process.env.ROOT_DIR}/badges/badge1.svg`, {"encoding": "utf8"})
+    //const svg = await badge.convertSvgToUri(svg1);
+    const res = await badge.addBadge("e_tech1", "Tech Badge", svg1);
+    await res.wait(1);
+    const mint = await badge.mintBadgeNFT(0);
+    mint.wait(1);
+
+    const user = await badge.ownerOf(0);
+    console.log(user)
+    
+    
+    
+    
+    
     
     
 
